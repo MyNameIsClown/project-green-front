e5e1fd308392e8fec2700f933faecc19114d70eb
MyNameIsClown <96545817+MyNameIsClown@users.noreply.github.com>
2023-06-14 23:59:43 +0200

    Finalizacion

--- constants.jsx
+++ constants.jsx
@@ -1,3 +1,3 @@
 // BACKEND
 export const LOCAL_URI = 'http://192.168.1.44:8090'
-export const REMOTE_URI = 'http://project-green-prod.eu-west-1.elasticbeanstalk.com'
+export const REMOTE_URI = 'http://coper.eu-west-1.elasticbeanstalk.com'
